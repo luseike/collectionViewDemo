42a80d0ef5502bc7330afbed5a49860ed7feddd4
jyl <jyl@jyldeiMac.local>
2014-11-05 11:37:45 +0800

    Initial Commit

---
+++ collectionViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  collectionViewDemo
+//
+//  Created by jyl on 14-11-5.
+//  Copyright (c) 2014年 jyl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ collectionViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  collectionViewDemo
+//
+//  Created by jyl on 14-11-5.
+//  Copyright (c) 2014年 jyl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
